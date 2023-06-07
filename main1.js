0b8b1a7f81067c5867a0690e90b37cf31b0dd21e
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-06-07 15:13:02 +0530

    Project_AsyncAwait_RestAPI_CrudCRud

---
+++ main1.js
@@ -0,0 +1,63 @@
+let products = []; // Array to store the product objects
+
+    let val = document.addEventListener('submit', upForm);
+
+    async function upForm(event) {
+      event.preventDefault();
+      let selling = document.getElementById('selling').value;
+      let product = document.getElementById('product').value;
+      let obj = {
+        sellingPrice: selling,
+        productName: product
+      };
+
+      try {
+        const response = await axios.post("https://crudcrud.com/api/b5e6a369e61f45e3a5ca8095f5cafe03/admin", obj);
+        uiUpdate(response.data);
+        calculateTotalValue(); // Update the total value
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    async function fetchProducts() {
+      try {
+        const response = await axios.get("https://crudcrud.com/api/b5e6a369e61f45e3a5ca8095f5cafe03/admin");
+        products = response.data; // Store the retrieved products in the array
+        for (let i = 0; i < response.data.length; i++) {
+          uiUpdate(response.data[i]);
+        }
+        calculateTotalValue(); // Update the total value
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    window.addEventListener('DOMContentLoaded', fetchProducts);
+
+    function uiUpdate(obj) {
+      let generateID = document.getElementById('outputList');
+      let showData = document.createElement('li');
+      showData.textContent = obj.sellingPrice + '-' + obj.productName;
+      let btn = document.createElement('button');
+      btn.textContent = 'delete button';
+      btn.onclick = async () => {
+        try {
+          await axios.delete(`https://crudcrud.com/api/b5e6a369e61f45e3a5ca8095f5cafe03/admin/${obj._id}`);
+          generateID.removeChild(showData);
+          products = products.filter(product => product._id !== obj._id); // Remove the product from the array
+          calculateTotalValue(); // Update the total value
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      generateID.appendChild(showData);
+      showData.appendChild(btn);
+    }
+    function calculateTotalValue() {
+        let totalValue = 0;
+        for (let i = 0; i < products.length; i++) {
+          totalValue += parseInt(products[i].selling);
+        }
+    }
